db2e7ddeabc5f518adae6808506b67f5981630e0
PriyadharshiniM1410 <m.priyadharshini141006@gmail.com>
2026-06-11 15:26:04 +0530

    Added health check endpoint

--- run.py
+++ run.py
@@ -2,5 +2,9 @@ from app import create_app
 
 app = create_app()
 
+@app.route('/healthz')
+def health_check():
+    return "OK", 200
+
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
